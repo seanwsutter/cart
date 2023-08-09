afd0e604c7a2482e9239ae6f7a523ef47a2de35c
seanwsutter <74794351+seanwsutter@users.noreply.github.com>
2023-08-09 17:58:50 -0500

    i think i did it

--- src/data.js
+++ src/data.js
@@ -4,27 +4,27 @@ let shopItemsData = [
     name: "Casual Shirt",
     price: 45,
     desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.",
-    img: "images/img1.JPG",
+    img: "images/imgone.png",
   },
   {
     id: "yboiii",
     name: "Office Shirt",
     price: 50,
     desc: "Lorem ipsum dolor  ",
-    img: "images/img2.JPG",
+    img: "images/imgtwo.png",
   },
   {
     id: "dawdaw",
     name: "T-Shirt",
     price: 25,
     desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.",
-    img: "images/img3.JPG",
+    img: "images/imgthree.png",
   },
   {
     id: "jaja",
     name: "Mens Suit",
     price: 200,
     desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.",
-    img: "images/img4.JPG",
+    img: "images/imgfour.png",
   }
 ];  
