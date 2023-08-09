1d0963d62a29d993bf13ee3a0e3b0c24b3bbf8d9
seanwsutter <74794351+seanwsutter@users.noreply.github.com>
2023-08-09 16:19:11 -0500

    fix3

--- src/data.js
+++ src/data.js
@@ -4,27 +4,27 @@ let shopItemsData = [
     name: "Casual Shirt",
     price: 45,
     desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.",
-    img: "images/img-1.jpg",
+    img: "images/img1.jpg",
   },
   {
     id: "yboiii",
     name: "Office Shirt",
     price: 50,
     desc: "Lorem ipsum dolor  ",
-    img: "images/img-2.jpg",
+    img: "images/img2.jpg",
   },
   {
     id: "dawdaw",
     name: "T-Shirt",
     price: 25,
     desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.",
-    img: "images/img-3.jpg",
+    img: "images/img3.jpg",
   },
   {
     id: "jaja",
     name: "Mens Suit",
     price: 200,
     desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.",
-    img: "images/img-4.jpg",
+    img: "images/img4.jpg",
   }
 ];  
